66cfb836393cd607eaaca6c1b84d17a0092ea697
endriwmsi <endriwmsi@gmail.com>
2024-11-20 21:34:24 -0300

    fix: create authOptions dedicated file

--- src/app/_actions/get-user-email.ts
+++ src/app/_actions/get-user-email.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "../_lib/auth";
 
 export async function getUserEmail(): Promise<string | null> {
   const session = await getServerSession(authOptions);
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,5 @@
-import { db } from "@/app/_lib/prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { Adapter } from "next-auth/adapters";
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async session({ session, user }) {
-      session.user = {
-        ...session.user,
-        id: user.id,
-      } as any;
-
-      return session;
-    },
-  },
-  secret: process.env.NEXT_AUTH_SECRET,
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/app/_lib/auth";
 
 const handler = NextAuth(authOptions);
 
