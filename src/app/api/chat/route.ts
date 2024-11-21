03e4fe0646e71c554980023278f03510714b4c2b
endriwmsi <endriwmsi@gmail.com>
2024-11-20 22:10:18 -0300

    fix: another solution by vercel

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,6 +2,7 @@ import { openai } from "@ai-sdk/openai";
 import { streamText } from "ai";
 
 // Allow streaming responses up to 30 seconds
+export const dynamic = "force-dynamic";
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
