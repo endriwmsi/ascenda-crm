c6b3986ec512e233bcfd7d04f28a2f7369ff9457
endriwmsi <endriwmsi@gmail.com>
2024-11-23 19:14:31 -0300

    chore: add new authentication layout

---
+++ src/app/authentication/layout.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Logo from "../_components/Logo";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import bgImage from "./../../../public/images/abstract.jpg";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/dashboard");
+    }
+  }, [status, router]);
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-background">
+      <div className="flex h-full w-full flex-col items-center justify-center gap-16 lg:w-1/2">
+        <div className="w-full max-w-[504px] px-8">{children}</div>
+      </div>
+
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex lg:w-1/2">
+        <div
+          className="absolute inset-0 bg-zinc-900"
+          style={{
+            backgroundImage: `url(${bgImage.src})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+
+        <div className="relative z-20 flex items-center justify-end text-lg font-medium">
+          <Logo />
+        </div>
+
+        <div className="relative z-20 mt-auto">
+          <blockquote className="space-y-2">
+            <p className="text-lg">
+              &ldquo;This library has saved me countless hours of work and
+              helped me deliver stunning designs to my clients faster than ever
+              before.&rdquo;
+            </p>
+            <footer className="text-sm">Sofia Davis</footer>
+          </blockquote>
+        </div>
+      </div>
+
+      {/* <div className="hidden h-full w-1/2 items-center justify-center shadow-lg lg:flex">
+        <div
+          className="h-screen w-full"
+          style={{
+            backgroundImage: `url(${bgImage.src})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+      </div> */}
+    </div>
+  );
+}
